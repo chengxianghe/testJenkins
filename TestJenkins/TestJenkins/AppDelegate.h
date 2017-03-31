4a438bd9f689c6fba6104b8a899349bc59a1171c
chengxianghe <chengxianghe@outlook.com>
2017-03-31 22:53:41 +0800

    init

---
+++ TestJenkins/TestJenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestJenkins
+//
+//  Created by chengxianghe on 2017/3/31.
+//  Copyright © 2017年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestJenkins/TestJenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestJenkins
+//
+//  Created by chengxianghe on 2017/3/31.
+//  Copyright © 2017年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
